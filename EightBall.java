4096a185c365561f16ce1a9ab0cbfc19753a140f
Haleh <hnematollahy@opentext.com>
2024-09-09 10:52:34 -0400

    Update EightBall.java

--- EightBall.java
+++ EightBall.java
@@ -10,6 +10,9 @@ public class EightBall {
 			System.out.println("Invalid input.");
 		}
 		new FileReader(filename).read(buffer);
+		/* new FileReader("fixed").read(buffer);
+		System.out.print(System.console().readPassword());
+		*/
 		System.out.println(buffer);
 	}
 }
